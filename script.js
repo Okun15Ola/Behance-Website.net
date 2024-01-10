8640de5966a76760d11aef5c0198866c05be7ea9
Okunola Olubanjo <okunolaolubanjo@gmail.com>
2024-01-10 08:51:12 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+
+var input = document.querySelector('input');
+
+input.addEventListener('click', border)
+function border() {
+    input.style.borderBottomColor= 'blue'
+    input.style.borderBottom= '2px solid blue'
+    
+}
